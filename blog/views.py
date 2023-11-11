0995e6d0f25d990122db82e6a21c57c131d7c3f0
antar <333antar333@gmail.com>
2023-11-11 07:21:49 +0100

    Added view blog_view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import time
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.cache import cache_page
+
+from blog.models import Blog
+
+
+@cache_page(5 * 60)
+def blog_view(request, blog_id: int):
+    time.sleep(3)
+    blog = get_object_or_404(Blog, id=blog_id)
+    return render(request, "blog.html", {"blog": blog})
